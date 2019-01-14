2c84a029dd314664f3388ac8b0dc02a91c1f8d36
Anjana Sarath <anjanasarath5@gmail.com>
2019-01-14 15:46:35 +0530

    bug@311

--- src/index.js
+++ src/index.js
@@ -2,32 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactSVGPanZoom from './viewer';
 
-export {default as ReactSVGPanZoom} from './viewer';
-export {default as Toolbar} from './ui-toolbar/toolbar';
-export {default as Miniature} from './ui-miniature/miniature';
-export {setPointOnViewerCenter, reset} from './features/common';
-export {pan} from './features/pan';
-export {zoom, fitSelection, fitToViewer, zoomOnViewerCenter} from './features/zoom';
-export {openMiniature, closeMiniature} from './features/miniature'
-export * from './constants';
+class Example extends React.Component{
 
-export const Viewer = () => (
-    	<ReactSVGPanZoom
+constructor(props) {
+    super(props);
+    this.state = {
+      x: 108,
+      y: 108.5,
+    };
+  	this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+
+  }
+
+  handleMouseMove(e){
+    console.log("hi")
+      const x = (e.offsetX - this.Viewer.getValue().e) / this.Viewer.getValue().a;
+      const y = (e.offsetY - this.Viewer.getValue().f) / this.Viewer.getValue().a;
+      this.setState({
+        x,
+        y,
+      })
+    };
+
+
+  handleMouseDown(e){
+    const mousemove = this.handleMouseMove;
+    const mouseup = this.handleMouseUp;
+    window.addEventListener('mousemove', mousemove);
+    window.addEventListener('mouseup', mouseup);
+  };
+
+  handleMouseUp() {
+    const mousemove = this.handleMouseMove;
+    const mouseup = this.handleMouseUp;
+      let value = Object.assign({}, this.Viewer.getValue());
+    if (this.state.x>value.SVGWidth) {
+        // If x-coordinate crosses svg-width then update svg-width
+        value.SVGWidth = this.state.x + 100;
+    }
+    if (this.state.y>value.SVGHeight) {
+            // If y-coordinate crosses svg-width then update svg-width
+            value.SVGHeight = this.state.y + 100;
+    }
+
+    this.Viewer.setValue(
+          value
+    );
+        window.removeEventListener('mousemove', mousemove);
+    window.removeEventListener('mouseup', mouseup);
+  };
+
+    render () {
+      return (
+        <ReactSVGPanZoom
+        ref={viewer => this.Viewer = viewer}
         width={500} height={500}
-        onClick={event => console.log(event.x, event.y, event.originalEvent)}>
-
-        <svg id='svg-area' width={617} height={316}>
+        detectAutoPan={false}
+      >
+        <svg width={617} height={316}>
             <circle
-              id='svg-circle'
-              cx={100}
-              cy={100}
-              r="70"
+              onMouseDown={this.handleMouseDown}
+              cx={this.state.x}
+              cy={this.state.y}
+              r="100"
               fill="#0ff"
               stroke="#0ff"
             />
         </svg>
       </ReactSVGPanZoom>
-  )
+    );
+  }
+ }
+
 
 ReactDOM.render(
-<Viewer />, document.getElementById('root'));
+  <div><Example /></div>,
+  document.getElementById('root')
+);
