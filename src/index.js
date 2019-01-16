a9a844d0a72f130cf6d47d4c0f33de9cdea888c7
Anjana Sarath <anjanasarath5@gmail.com>
2019-01-17 04:13:28 +0530

    bug-fix: mobile-svg

--- src/index.js
+++ src/index.js
@@ -2,82 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactSVGPanZoom from './viewer';
 
-class Example extends React.Component{
+export {default as ReactSVGPanZoom} from './viewer';
+export {default as Toolbar} from './ui-toolbar/toolbar';
+export {default as Miniature} from './ui-miniature/miniature';
+export {setPointOnViewerCenter, reset} from './features/common';
+export {pan} from './features/pan';
+export {zoom, fitSelection, fitToViewer, zoomOnViewerCenter} from './features/zoom';
+export {openMiniature, closeMiniature} from './features/miniature'
+export * from './constants';
 
-constructor(props) {
-    super(props);
-    this.state = {
-      x: 108,
-      y: 108.5,
-    };
-  	this.handleMouseDown = this.handleMouseDown.bind(this);
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-    this.handleMouseUp = this.handleMouseUp.bind(this);
+export class Viewer extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        radius:70,
+        right: 600,
+        bottom: 300,
+        x: 108,
+        y: 108.5,
+      };
+    	this.initDrag = this.initDrag.bind(this);
+      this.drag = this.drag.bind(this);
+      this.endDrag = this.endDrag.bind(this);
+    }
 
+drag(evt){
+  const r = this.state.radius;
+  const initWidth= 600;
+  const initHeight = 300;
+  const x = (evt.offsetX - this.Viewer.getValue().e) / this.Viewer.getValue().a;
+  const y = (evt.offsetY - this.Viewer.getValue().f) / this.Viewer.getValue().a;
+
+  //Update x and right when circle moves along x-axis
+  if(x+r > initWidth) {
+    this.setState({x,right:x+r});
+  } else if(x-r > 0) {
+    this.setState({x});
   }
 
-  handleMouseMove(e){
-    console.log("hi")
-      const x = (e.offsetX - this.Viewer.getValue().e) / this.Viewer.getValue().a;
-      const y = (e.offsetY - this.Viewer.getValue().f) / this.Viewer.getValue().a;
-      this.setState({
-        x,
-        y,
-      })
-    };
+  //Update y and bottom when circle moves along y-axis
+  if(y+r > initHeight) {
+    this.setState({y,bottom:y+r});
+  } else if(y-r > 0) {
+    this.setState({y});
+  }
+}
 
+initDrag(evt){
+  window.addEventListener('mousemove', this.drag);
+  window.addEventListener('mouseup',this.endDrag);
+};
 
-  handleMouseDown(e){
-    const mousemove = this.handleMouseMove;
-    const mouseup = this.handleMouseUp;
-    window.addEventListener('mousemove', mousemove);
-    window.addEventListener('mouseup', mouseup);
-  };
-
-  handleMouseUp() {
-    const mousemove = this.handleMouseMove;
-    const mouseup = this.handleMouseUp;
-      let value = Object.assign({}, this.Viewer.getValue());
-    if (this.state.x>value.SVGWidth) {
-        // If x-coordinate crosses svg-width then update svg-width
-        value.SVGWidth = this.state.x + 100;
-    }
-    if (this.state.y>value.SVGHeight) {
-            // If y-coordinate crosses svg-width then update svg-width
-            value.SVGHeight = this.state.y + 100;
-    }
-
-    this.Viewer.setValue(
-          value
-    );
-        window.removeEventListener('mousemove', mousemove);
-    window.removeEventListener('mouseup', mouseup);
-  };
-
-    render () {
-      return (
-        <ReactSVGPanZoom
+endDrag() {
+  window.removeEventListener('mousemove', this.drag);
+  window.removeEventListener('mouseup', this.endDrag);
+};
+  render(){
+    return (
+    	<ReactSVGPanZoom
         ref={viewer => this.Viewer = viewer}
         width={500} height={500}
-        detectAutoPan={false}
       >
-        <svg width={617} height={316}>
+        <svg id='svg-area' width={this.state.right} height={this.state.bottom}>
             <circle
-              onMouseDown={this.handleMouseDown}
+              onMouseDown={this.initDrag}
               cx={this.state.x}
               cy={this.state.y}
-              r="100"
+              id='svg-circle'
+              r={this.state.radius}
               fill="#0ff"
               stroke="#0ff"
             />
         </svg>
       </ReactSVGPanZoom>
-    );
-  }
- }
-
+  )
+}
+}
 
 ReactDOM.render(
-  <div><Example /></div>,
-  document.getElementById('root')
-);
+<Viewer />, document.getElementById('root'));
